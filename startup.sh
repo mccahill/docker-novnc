b9cfa8b20992a12f67dd63c14fc2ef4c3f29657c
Mark McCahill <mark.mccahill@duke.edu>
2015-07-06 16:27:16 -0400

    new, improved generic noVNC OpenBox template
    now with the great taste of fish!
    
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- startup.sh
+++ startup.sh
@@ -4,3 +4,4 @@ Xvfb :1 -screen 0 1280x1024x16 &
 openbox-session&
 x11vnc -display :1 -bg -nopw -listen localhost -xkb -ncache 10 -ncache_cr -rfbauth /root/.vnc/passwd
 cd /root/noVNC && ./utils/launch.sh --vnc localhost:5900
+
